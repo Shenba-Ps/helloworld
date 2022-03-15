349bc02a19e5ec94152c9af4b4e030b2a24253f8
Shenba-ps <ssrcooling@gmail.com>
2022-03-15 11:19:30 +0530

    first commit

---
+++ Intro/Comparison.java
@@ -0,0 +1,24 @@
+class Comparison{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if (a>b){
+            System.out.println("print a is greater than b");
+        }
+        if (a<b){
+            System.out.println("print a is less than b");
+        }
+        if (a>=b){
+            System.out.println("print a is greater than or eqaul to b");
+        }
+        if (a<=b){
+            System.out.println("print a is less than or equal to b");
+        }
+        if (a==b){
+            System.out.println("print a is equal to b");
+        }
+        if (a!=b){
+            System.out.println("print a is not equal to b");
+        }
+    }
+}
